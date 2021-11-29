980c33728c01370c5dfa1ac2002a65cda8c62167
NaHyeon Kim <nahyeonee99@gmail.com>
2021-11-29 18:54:35 +0900

    day 11/29 in class

---
+++ day 11.29/문제1.js
@@ -0,0 +1,21 @@
+let text = "";
+let count = 0;
+
+for (let i = 0; i < 10001; i++) {
+  text += i;
+}
+
+console.log(text);
+
+for (let i = i; i < text.length; i++) {
+  if (text[i] == "8") {
+    count += 1;
+  }
+}
+
+console.log(count);
+
+// function countEight (number) {
+//   let array = Array.from(new Array(number), (x,i) => i + 1)
+//   return array.join("").split("8").length-1;
+// }
