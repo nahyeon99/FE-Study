755c0b4a0da6604ba905e72889841ecf6dd2a1b1
Nahyeon Kim <nahyeonee99@gmail.com>
2022-01-10 09:48:27 +0900

    [day 0110] react #2

---
+++ App.js
@@ -0,0 +1,24 @@
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+function Hello(props) {
+  const name = props.name;
+  const num = [1, 2, 3, 4, 5];
+
+  const numComArr = num.map((i) => (
+    <h1 key={i.toString()}>
+      안녕, {name}, {i}호
+    </h1>
+  ));
+
+  return <div>{numComArr}</div>;
+}
+
+export default App;
