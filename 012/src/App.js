d1cfaff700bcd57f4dc768106c9a8186075f9924
Nahyeon Kim <nahyeonee99@gmail.com>
2022-01-11 20:30:27 +0900

    [day 0111] react final

---
+++ 012/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const App = () => {
+  return <HelloLicat value={{ name: "gary", id: "garyIsFree" }} />;
+};
+
+const HelloLicat = (props) => {
+  const id = props.value.id;
+  const name = props.value.name;
+  return (
+    <div>
+      <h2>{id}</h2>
+      <strong>{name}</strong>
+      <HelloLicatTwo value={{ id, name }} />
+    </div>
+  );
+};
+
+const HelloLicatTwo = (props) => {
+  const id = props.value.id;
+  const name = props.value.name;
+  return (
+    <div>
+      <h2>Two : {id}</h2>
+      <strong>Two : {name}</strong>
+    </div>
+  );
+};
+
+export default App;
